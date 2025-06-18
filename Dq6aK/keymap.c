e27a191481a0e728fd2e905d1baad6034bb209cd
Miki Lamberg <ml227cr@student.lnu.se>
2025-06-18 14:05:41 +0200

    Update keymap.c
    Add Archordion

--- Dq6aK/keymap.c
+++ Dq6aK/keymap.c
@@ -1,14 +1,19 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "i18n.h"
+#include "features/achordion.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
+
+
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
 
-
+void housekeeping_task_user(void) {
+  achordion_task();
+}
 
 enum tap_dance_codes {
   DANCE_0,
@@ -124,6 +129,7 @@ bool rgb_matrix_indicators_user(void) {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
 
     case RGB_SLD:
